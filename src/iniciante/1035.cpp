18fe2ae94a6c47e90d84b7bc7a9d547dc5c2e1bf
Felipe Menino <efelipecarlos@gmail.com>
2018-02-12 10:44:54 -0200

    Correção do exercício 1035

--- src/iniciante/1035.cpp
+++ src/iniciante/1035.cpp
@@ -3,34 +3,16 @@
 
 using namespace std;
 
-int maior(int x, int y) {
-	return (x + y + abs(x - y)) / 2;	
-}
-
-bool parPositivo(int x, int y) {
-	bool par = false;
-	if (((x % 2 == 0)) and x >= 0)
-		par = true;
-	else if (((y % 2) == 0) and y >= 0)
-		par = true;
-	
-	return par;
-}
-
 int main() {
 	int a, b, c, d;
-	int maiorBC, maiorDA, somaCD, somaAB;
+	string msg = "Valores nao aceitos";
 
 	cin >> a >> b >> c >> d;
-	maiorBC = maior(b, c); maiorDA = maior(d, a); 
-	somaCD = c + d; somaAB = a + b; 
 	
-	if (parPositivo(c, d)) {
-		if (maiorBC == b and maiorDA == d and somaCD > somaAB) 
-			cout << "Valores aceitos" << endl;
-		else 
-			cout << "Valores nao aceitos" << endl;
-	} else 
-		cout << "Valores nao aceitos" << endl;
-
+	if (b > c && d > a)
+		if ((c + d) > (a + b))
+			if (c >= 0 && d >= 0)
+				if ((a % 2) == 0)
+					msg = "Valores aceitos";
+	cout << msg << endl;
 }
